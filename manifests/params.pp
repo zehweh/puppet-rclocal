4cda5a2515f29b0fa8cf4137ab2235000a582324
zehweh <cw@tf2.net>
2024-10-16 12:30:09 +0200

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,12 +8,12 @@ class rclocal::params  {
 ## MODULE INTERNAL VARIABLES
 # (Modify to adapt to unsupported OSes)
 
-  $config_file = $::osfamily ? {
+  $config_file = $facts['os']['family'] ? {
     'RedHat'  => '/etc/rc.d/rc.local',
     default => '/etc/rc.local',
   }
 
-  $config_dir = $::operatingsystem ? {
+  $config_dir = $facts['os']['family'] ? {
     default => '/etc/rc.local.d',
   }
 
